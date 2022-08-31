a5e78054505ef2a986e10597a80d87d63035e5a0
Admin <anstdor@gmail.com>
2022-08-31 20:20:39 +0300

    HW 10 is done

--- src/homework9/Book.java
+++ src/homework9/Book.java
@@ -1,5 +1,7 @@
 package homework9;
 
+import java.util.Objects;
+
 public class Book {
     private String bookTitle;
     private int publishingYear;
@@ -14,11 +16,35 @@ public class Book {
     public String getBookTitle() {
         return bookTitle;
     }
+
     public int getPublishingYear() {
         return publishingYear;
     }
+
     public void setPublishingYear(int publishingYear) {
         this.publishingYear = publishingYear;
     }
 
+    public homework9.Author getAuthor() {
+        return author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publishingYear == book.publishingYear && bookTitle.equals(book.bookTitle) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookTitle, publishingYear, author);
+    }
+
+    @Override
+    public String toString() {
+        return bookTitle + " " + author + " " + publishingYear;
+    }
 }
+
--- src/homework9/Homework9.java
+++ src/homework9/Homework9.java
@@ -16,9 +16,11 @@ public class Homework9 {
         book.setPublishingYear(2005);
 
 
-        System.out.println("Book title - " + bookTitle);
-        System.out.println("Publishing year - " + book.getPublishingYear());
-        System.out.println("Author - " + authorFirstName + authorSecondName);
+        //System.out.println("Book title - " + bookTitle);
+        //System.out.println("Publishing year - " + book.getPublishingYear());
+        //System.out.println("Author - " + authorFirstName + authorSecondName);
+        System.out.println(book.getBookTitle() + " " + book.getAuthor().getFirstName() + " " + book.getAuthor().getSecondName() + " " + book.getPublishingYear());
+        System.out.println(book);
 
         System.out.println();
         System.out.println("Second book");
@@ -37,5 +39,8 @@ public class Homework9 {
 
         book2.setPublishingYear(2001);
         System.out.println("New publishing year - " + book2.getPublishingYear());
+
+        System.out.println(book.hashCode() == book2.hashCode());
+        System.out.println(book.equals(book2));
     }
 }
